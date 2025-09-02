b5bc033222f9bc219bb56d6fba18c9de2c88292a
frimi01 <82447464+Frimi01@users.noreply.github.com>
2025-09-02 19:10:08 +0200

    changed displayManager and installed gpu driver
    i do not remember why i did this

--- nixos/hosts/nixos/configuration.nix
+++ nixos/hosts/nixos/configuration.nix
@@ -52,8 +52,12 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.displayManager.gdm.wayland = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Intel GPU driver
+  # services.videoDrivers = [ "intel" ];
 
   # Configure keymap in X11
   services.xserver.xkb = {
