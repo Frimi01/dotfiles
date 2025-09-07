88e97018df5d8344293004e4dfba4bad487e1c91
frimi01 <82447464+Frimi01@users.noreply.github.com>
2025-09-07 19:29:22 +0200

    idle fixes

--- nixos/hosts/nixos/home.nix
+++ nixos/hosts/nixos/home.nix
@@ -57,7 +57,30 @@
     };
   };
 
-  services.hypridle.enable = true;
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
+        lock_cmd = "hyprlock";
+      };
+
+      listener = [
+        {
+          timeout = 60;
+          on-timeout = "hyprlock";
+        }
+        {
+          timeout = 90;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
+    };
+  };
+
+
   services.hyprpaper = {
     enable = true;
     settings = {
