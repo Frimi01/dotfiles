3b88f4ac5f75f52e38afd80ce1411c14ae792a72
frimi01 <82447464+Frimi01@users.noreply.github.com>
2025-09-11 13:07:38 +0200

    updated timings

--- nixos/hosts/nixos/home.nix
+++ nixos/hosts/nixos/home.nix
@@ -72,12 +72,12 @@
           on-timeout = "hyprlock";
         }
         {
-          timeout = 90;
+          timeout = 120;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
         {
-          timeout = 100;
+          timeout = 1800;
           on-timeout = "systemctl suspend";
         }
       ];
